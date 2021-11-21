71d657979a6bdf13ce00701d9ba2658aef76514d
twiherhol@gmail.com <1904129049@qq.com>
2021-11-21 09:22:04 +0800

    hot-fix commit

--- src/main/java/fixTest.java
+++ src/main/java/fixTest.java
@@ -2,4 +2,7 @@ public class fixTest {
     public void somethingAboutTest(){
         System.out.println("branch test");
     }
+    public void mergeMasterTest(){
+        System.out.println("merge Master Try");
+    }
 }
